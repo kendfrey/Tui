5c1ac377a639fd9f15cbc905a24556b5e34e145c
Kendall Frey <kendfrey@gmail.com>
2015-01-03 17:06:47 -0500

    Documentation for Timer

--- Tui/Timer.cs
+++ Tui/Timer.cs
@@ -6,13 +6,24 @@ using System.Threading.Tasks;
 
 namespace Tui
 {
+    /// <summary>
+    /// Allows firing events at specific times.
+    /// </summary>
     public class Timer
     {
         System.Timers.Timer timer;
         Screen screen;
 
+        /// <summary>
+        /// Occurs when the interval since the last tick has elapsed.
+        /// </summary>
         public event EventHandler Tick;
 
+        /// <summary>
+        /// Initializes and starts a new timer.
+        /// </summary>
+        /// <param name="interval">The interval between ticks.</param>
+        /// <param name="screen">The screen containing the event loop used to process the ticks.</param>
         public Timer(TimeSpan interval, Screen screen)
         {
             this.screen = screen;
@@ -26,6 +37,10 @@ namespace Tui
             screen.PushEvent(() => OnTick(new EventArgs()));
         }
 
+        /// <summary>
+        /// Raises the Tick event.
+        /// </summary>
+        /// <param name="e">The event data to pass to the event.</param>
         protected virtual void OnTick(EventArgs e)
         {
             EventHandler tick = Tick;
