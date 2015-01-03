1a505e76fe4fa1133be1ec984a763a9eb81a2e20
Kendall Frey <kendfrey@gmail.com>
2015-01-03 17:49:13 -0500

    Documentation for CharData

--- Tui/CharData.cs
+++ Tui/CharData.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Tui
 {
+    /// <summary>
+    /// Represents a character cell containing a character, foreground color, and background color.
+    /// </summary>
     public struct CharData
     {
         static CharData()
@@ -13,6 +16,12 @@ namespace Tui
             Encoding = Encoding.GetEncoding(437);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the CharData structure with the specified character and colors.
+        /// </summary>
+        /// <param name="character">The character to display.</param>
+        /// <param name="foreground">The foreground color.</param>
+        /// <param name="background">The background color.</param>
         public CharData(char character, TextColor foreground, TextColor background) : this()
         {
             Character = character;
@@ -20,6 +29,12 @@ namespace Tui
             Background = background;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the CharData structure with the specified character and colors.
+        /// </summary>
+        /// <param name="characterByte">The byte value of the character to display.</param>
+        /// <param name="foreground">The foreground color.</param>
+        /// <param name="background">The background color.</param>
         public CharData(byte characterByte, TextColor foreground, TextColor background) : this()
         {
             CharacterByte = characterByte;
@@ -33,11 +48,18 @@ namespace Tui
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the byte value of the character to display, using code page 437.
+        /// </summary>
         public byte CharacterByte
         {
             get;
             set;
         }
+
+        /// <summary>
+        /// Gets or sets the character to display.
+        /// </summary>
         public char Character
         {
             get
@@ -49,11 +71,19 @@ namespace Tui
                 CharacterByte = Encoding.GetBytes(new char[] { value })[0];
             }
         }
+
+        /// <summary>
+        /// Gets or sets the color to display the character in.
+        /// </summary>
         public TextColor Foreground
         {
             get;
             set;
         }
+
+        /// <summary>
+        /// Gets or sets the color to display the background in.
+        /// </summary>
         public TextColor Background
         {
             get;
