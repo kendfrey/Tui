9788e0d368a6e81099f0fd370c189b5e2484f82e
Kendall Frey <kendfrey@gmail.com>
2015-01-03 20:11:19 -0500

    Documentation for DisplayMode

--- Tui/DisplayMode.cs
+++ Tui/DisplayMode.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace Tui
 {
+    /// <summary>
+    /// Specifies the way a screen will be displayed.
+    /// </summary>
     public enum DisplayMode
     {
+        /// <summary>
+        /// The screen will be displayed in a window.
+        /// </summary>
         FixedWindow,
+
+        /// <summary>
+        /// The screen will be displayed in a window with a draggable resize border.
+        /// </summary>
         ResizableWindow,
+
+        /// <summary>
+        /// The screen will be displayed in fullscreen, with the character dimensions changed to fit the screen.
+        /// </summary>
         Fullscreen,
+
+        /// <summary>
+        /// The screen will be displayed in fullscreen, with the characters scaled to fit the screen.
+        /// </summary>
         FixedFullscreen
     }
 }
