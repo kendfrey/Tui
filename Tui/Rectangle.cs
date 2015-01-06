f9a5664481c9d45a42f0557f6b032e8592009169
Kendall Frey <kendfrey@gmail.com>
2015-01-05 19:40:29 -0500

    Documentation for Rectangle

--- Tui/Rectangle.cs
+++ Tui/Rectangle.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace Tui
 {
+    /// <summary>
+    /// Represents a rectangular area on a screen.
+    /// </summary>
     public struct Rectangle
     {
+        /// <summary>
+        /// Initializes a new instance of the Rectangle structure with the specified dimensions.
+        /// </summary>
+        /// <param name="x">The X coordinate of the top-left corner of the rectangle.</param>
+        /// <param name="y">The Y coordinate of the top-left corner of the rectangle.</param>
+        /// <param name="width">The width of the rectangle.</param>
+        /// <param name="height">The height of the rectangle.</param>
         public Rectangle(int x, int y, int width, int height) : this()
         {
             X = x;
@@ -16,24 +26,36 @@ namespace Tui
             Height = height;
         }
 
+        /// <summary>
+        /// Gets or sets the X coordinate of the top-left corner of the rectangle.
+        /// </summary>
         public int X
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the Y coordinate of the top-left corner of the rectangle.
+        /// </summary>
         public int Y
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the width of the rectangle.
+        /// </summary>
         public int Width
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Gets or sets the height of the rectangle.
+        /// </summary>
         public int Height
         {
             get;
