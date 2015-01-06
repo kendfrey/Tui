a10cba748029762ffc4d9e914850dbf3e536d15f
Kendall Frey <kendfrey@gmail.com>
2015-01-05 19:54:27 -0500

    Documentation for TextColor

--- Tui/TextColor.cs
+++ Tui/TextColor.cs
@@ -6,23 +6,89 @@ using System.Threading.Tasks;
 
 namespace Tui
 {
+    /// <summary>
+    /// Specifies a color for rendering text.
+    /// </summary>
     public enum TextColor : byte
     {
+        /// <summary>
+        /// Black, or the first color in a custom palette.
+        /// </summary>
         Black,
+
+        /// <summary>
+        /// Dark red, or the second color in a custom palette.
+        /// </summary>
         DarkRed,
+
+        /// <summary>
+        /// Dark green, or the third color in a custom palette.
+        /// </summary>
         DarkGreen,
+
+        /// <summary>
+        /// Dark yellow, or the fourth color in a custom palette.
+        /// </summary>
         DarkYellow,
+
+        /// <summary>
+        /// Dark blue, or the fifth color in a custom palette.
+        /// </summary>
         DarkBlue,
+
+        /// <summary>
+        /// Dark magenta, or the sixth color in a custom palette.
+        /// </summary>
         DarkMagenta,
+
+        /// <summary>
+        /// Dark cyan, or the seventh color in a custom palette.
+        /// </summary>
         DarkCyan,
+
+        /// <summary>
+        /// Light gray, or the eighth color in a custom palette.
+        /// </summary>
         LightGray,
+
+        /// <summary>
+        /// Medium gray, or the ninth color in a custom palette.
+        /// </summary>
         Gray,
+
+        /// <summary>
+        /// Red, or the tenth color in a custom palette.
+        /// </summary>
         Red,
+
+        /// <summary>
+        /// Green, or the eleventh color in a custom palette.
+        /// </summary>
         Green,
+
+        /// <summary>
+        /// Yellow, or the twelfth color in a custom palette.
+        /// </summary>
         Yellow,
+
+        /// <summary>
+        /// Blue, or the thirteenth color in a custom palette.
+        /// </summary>
         Blue,
+
+        /// <summary>
+        /// Magenta, or the fourteenth color in a custom palette.
+        /// </summary>
         Magenta,
+
+        /// <summary>
+        /// Cyan, or the fifteenth color in a custom palette.
+        /// </summary>
         Cyan,
+
+        /// <summary>
+        /// White, or the sixteenth color in a custom palette.
+        /// </summary>
         White
     }
 }
